659687572cb0dee497b204046997bbe9e583dc5f
admin <admin@example.com>
2020-03-19 09:52:52 -0700

    Change hello message

--- src/main/java/com/evilcorp/Main.java
+++ src/main/java/com/evilcorp/Main.java
@@ -2,6 +2,6 @@ package com.evilcorp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello all!!!");
     }
 }
